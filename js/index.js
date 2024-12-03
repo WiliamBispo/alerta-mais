0cce8d6dc553a66dd896f42b29e1b419ce4a7084
Angélica Anjos <angellica.anjos@hotmail.com>
2024-12-03 20:05:46 -0300

    Alteração do ponto no mapa

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const map = L.map('map').setView([-12.9310, -38.4095], 12); 
+    const map = L.map('map').setView([-12.9310, -38.4095], 12);
 
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors © CartoDB',
     }).addTo(map);
 
-    L.marker([-12.9714, -38.5014]) 
-        .addTo(map)
-        .bindPopup('<b>Salvador - Bahia</b><br>Capital da Bahia.')
-        .openPopup();
+    L.circleMarker([-12.9714, -38.5014], {
+        color: '#004CBF',
+        fillColor: '#015CE1', 
+        fillOpacity: 0.9,
+        radius: 6 
+    }).addTo(map).bindPopup('<b>Salvador - Bahia</b>');
 });
-
-
